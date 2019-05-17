2a6ea21152881754bf35a52d77b70f344321485c
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-17 14:00:55 +0100

    Comparisons with the Strict Equality Operator
    Strict equality (===) is the counterpart to the equality operator (==). However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion.
    
    If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
    
    Examples
    
    3 === 3 // true
    3 === '3' // false
    In the second example, 3 is a Number type and '3' is a String type.

---
+++ Comparison-with-the-Strict-Equality-Operator.js
@@ -0,0 +1,8 @@
+// Use the strict equality operator in the if statement so the function will return "Equal" when val is strictly equal to 7
+
+function testStrict(val) {
+if(val === 7) {
+return "Equal";
+}
+return "Not Equal"
+}
