2a2de45c3a287b0d84c173b316c924eb645bcbd8
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-17 15:51:03 +0100

    Comparisons with the Greater Than Or Equal To Operator
    The greater than or equal to operator (>=) compares the values of two numbers. If the number to the left is greater than or equal to the number to the right, it returns true. Otherwise, it returns false.
    
    Like the equality operator, greater than or equal to operator will convert data types while comparing.
    
    Examples
    
    6 >= 6 // true
    7 >= '3' // true
    2 >= 3 // false
    '7' >= 9 // false

---
+++ Comparison-with-the-Greater-Than-Or-Equal-To-Operator.js
@@ -0,0 +1,9 @@
+// Add the greater than or equal to operator to the indicated lines so that the return statements make sense.
+
+function testGreaterOrEqual(val) {
+if(val >= 20) {
+return "20 or over";
+}
+if(val >= 10) {
+return "10 or over";
+}
