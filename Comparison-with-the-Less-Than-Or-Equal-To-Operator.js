523ff75a0fc352ea4d13c02630f409425ebf6efc
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-17 16:04:10 +0100

    Comparisons with the Less Than Or Equal To Operator
    The less than or equal to operator (<=) compares the values of two numbers. If the number to the left is less than or equal to the number to the right, it returns true. If the number on the left is greater than the number on the right, it returns false. Like the equality operator, less than or equal to converts data types.
    
    Examples
    
    4 <= 5 // true
    '7' <= 7 // true
    5 <= 5 // true
    3 <= 2 // false
    '8' <= 4 // false

---
+++ Comparison-with-the-Less-Than-Or-Equal-To-Operator.js
@@ -0,0 +1,9 @@
+// Add the less than or equal to operator to the indicated lines so that the return statements make sense.
+
+function testLessOrEqual(val) {
+if(val <= 12) {
+return "Smaller Than Or Equal To 12";
+}
+if(val <= 24) {
+return "Smaller Than Or Equal To 24";
+}
