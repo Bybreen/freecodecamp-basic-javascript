641655c0abbe5fc6772937b7587553fb7b43ed06
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-17 14:11:41 +0100

    Comparison with the Inequality Operator
    The inequality operator (!=) is the opposite of the equality operator. It means "Not Equal" and returns false where equality would return true and vice versa. Like the equality operator, the inequality operator will convert data types of values while comparing.
    
    Examples
    
    1 != 2 // true
    1 != "1" // false
    1 != '1' // false
    1 != true // false
    0 != false // false

---
+++ Comparison-with-the-Inequality-Operator.js
@@ -0,0 +1,9 @@
+// Add the inequality operator != in the if statement so that the function will return "Not Equal" 
+when val is not equivalent to 99
+
+function testNotEqual(val) {
+if(val != 99) {
+return "Not Equal";
+}
+return "Equal";
+}
