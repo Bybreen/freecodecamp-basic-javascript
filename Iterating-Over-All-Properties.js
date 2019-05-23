51b84353551bd59e8e71061c70965368c7a721c6
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-05-23 16:18:57 +0100

    Iterate Over All Properties
    You have now seen two kinds of properties: own properties and prototype properties. Own properties are defined directly on the object instance itself. And prototype properties are defined on the prototype.
    
    function Bird(name) {
      this.name = name; //own property
    }
    
    Bird.prototype.numLegs = 2; // prototype property
    
    let duck = new Bird("Donald");
    Here is how you add duck’s own properties to the array ownProps and prototype properties to the array prototypeProps:
    
    let ownProps = [];
    let prototypeProps = [];
    
    for (let property in duck) {
      if(duck.hasOwnProperty(property)) {
        ownProps.push(property);
      } else {
        prototypeProps.push(property);
      }
    }
    
    console.log(ownProps); // prints ["name"]
    console.log(prototypeProps); // prints ["numLegs"]

---
+++ Iterating-Over-All-Properties.js
@@ -0,0 +1,23 @@
+// Add all of the own properties of beagle to the array ownProps. 
+// Add all of the prototype properties of Dog to the array prototypeProps.
+
+function Dog(name) {
+this.name = name;
+}
+
+Dog.proptotype.numLegs = 4;
+
+let.beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+if(beagle.ownProperty(property)) {
+
+ownProps.push(property);
+} else {
+prototypeProps.push(property);
+}
+}
+
